300e028bbe65bc7660a89684b227b2403b105605
Franco <francomartin.personal@gmail.com>
2021-08-03 23:08:43 -0300

    router added

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const Dog = require('../models/Dog')
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/dog', Dog)
 
 module.exports = router;
